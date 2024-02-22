4caf2b3cbd18aa361350654df07dea4b893d2e34
hamza422 <hamza.jamil422@gmail.com>
2024-02-22 18:48:52 +0100

    create spacecraft controller class

---
+++ LocationService/src/main/java/com/example/locationservice/spacecraftEvent/spacecraftEventController.java
@@ -0,0 +1,9 @@
+package com.example.locationservice.spacecraftEvent;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api/spacecraftEvent")
+public class spacecraftEventController {
+}

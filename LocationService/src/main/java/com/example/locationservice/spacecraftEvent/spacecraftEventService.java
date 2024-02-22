286dd62b30e3f2437304d2e9887e4de758658a3c
hamza422 <hamza.jamil422@gmail.com>
2024-02-22 18:55:02 +0100

    fetch all the json file data and store in classes

--- LocationService/src/main/java/com/example/locationservice/spacecraftEvent/spacecraftEventService.java
+++ LocationService/src/main/java/com/example/locationservice/spacecraftEvent/spacecraftEventService.java
@@ -1,7 +1,36 @@
 package com.example.locationservice.spacecraftEvent;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class spacecraftEventService {
+    private List<Latitude> listOfLatitutes;
+    private  List<Longitude> listOfLongitudes;
+    private  List<SpacecraftEvent> listOfEvents;
+
+    private List<CombinedEventData> listOfCombineData;
+
+    private static final String LATITUDE_FILENAME="src/main/resources/files/latitudes.json";
+    private static final String LONGITUDE_FILENAME="src/main/resources/files/longitudes.json";
+    private static final String EVENT_FILENAME="src/main/resources/files/events.json";
+
+    public spacecraftEventService() throws IOException {
+        fetchAllData();
+    }
+
+    private void fetchAllData() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        listOfLatitutes = mapper.readValue(new File(LATITUDE_FILENAME), new TypeReference<>(){});
+        listOfLongitudes = mapper.readValue(new File(LONGITUDE_FILENAME), new TypeReference<>(){});
+        listOfEvents= mapper.readValue(new File(EVENT_FILENAME), new TypeReference<>(){});
+
+    }
 }

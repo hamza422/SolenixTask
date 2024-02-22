234ed7803a8fd416c6fe561a8fdcbf39194d8693
hamza422 <hamza.jamil422@gmail.com>
2024-02-22 18:51:43 +0100

    add dependency injection for service

--- LocationService/src/main/java/com/example/locationservice/spacecraftEvent/spacecraftEventController.java
+++ LocationService/src/main/java/com/example/locationservice/spacecraftEvent/spacecraftEventController.java
@@ -1,9 +1,18 @@
 package com.example.locationservice.spacecraftEvent;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(path="/api/spacecraftEvent")
 public class spacecraftEventController {
+    private final spacecraftEventService spacecraftEventService;
+
+    @Autowired
+    public spacecraftEventController(spacecraftEventService spacecraftEventService) throws IOException {
+        this.spacecraftEventService = spacecraftEventService;
+    }
 }
